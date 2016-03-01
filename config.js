e042dbdeadbf29b92e8c3fca47f6379f2688b240
aaronice <tercelhuang@gmail.com>
2016-03-01 01:49:29 -0800

    Add config for local mongodb or mongolab connection

--- config.js
+++ config.js
@@ -8,5 +8,10 @@ config.targetUrl = "www.xiachufang.com";
 config.initialPort = 80;
 config.initialPath = "/";
 
+config.mongodb = {};
+config.mongodb.username = "pikachu";
+config.mongodb.password = "";
+config.mongodb.dburl = "";
+
 
 module.exports = config;
