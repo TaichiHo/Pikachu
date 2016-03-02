1023144d38c80a1f018a8e3a2c4dcd72758f8535
aaronice <tercelhuang@gmail.com>
2016-03-02 02:03:55 -0800

    Add deployment files and mLab mongodb VCAP_SERVICES

--- config.js
+++ config.js
@@ -9,9 +9,25 @@ config.initialPort = 80;
 config.initialPath = "/";
 
 config.mongodb = {};
-config.mongodb.username = "pikachu";
-config.mongodb.password = "";
-config.mongodb.dburl = "";
 
+// VCAP_SERVICES
+if (process.env.VCAP_SERVICES) {
+  var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+
+  if (vcapServices['user-provided'] != null) {
+    var userProvidedServices = vcapServices['user-provided'];
+
+    for (var i = 0; i < userProvidedServices.length; i++) {
+      if (userProvidedServices[i].name == 'mLab') {
+        config.mongodb.dburl = userProvidedServices[i].credentials.mongodburi;
+        console.log("Using mLab MongoDB");
+      }
+    }
+  }
+}
+
+if (!config.mongodb.dburl) {
+  config.mongodb.dburl = 'mongodb://localhost:27017/pikachu'
+}
 
 module.exports = config;
--- services/MongoDBClient.js
+++ services/MongoDBClient.js
@@ -4,8 +4,8 @@ var config = require('../config');
 
 var client = false;
 var dbURI =
-  process.env.MONGOLAB_URI ||
   config.mongodb.dburl ||
+  process.env.MONGOLAB_URI ||
   'mongodb://localhost:27017/pikachu';
 
 var MongoDBClient = function() {};
